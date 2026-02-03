c807233276f303af0e2ffb2510466e59f24218fc
LisbethCifuentes <96801137+LisbethCifuentes@users.noreply.github.com>
2026-02-02 20:58:17 -0500

    version 1.2

--- 0 Tareas de clase/06. App_Pet - Colas/pets/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-# No es necesario registrar modelos de MongoEngine aquí
-# porque no usan el admin de Django de la misma manera
-
-# Si quieres ver las mascotas en el admin, necesitarías
-# una librería adicional como django-mongoengine
-# Por ahora, este archivo puede quedar así o vacío
--- 0 Tareas de clase/06. App_Pet - Colas/pets/models.py
+++
@@ -1,8 +0,0 @@
-from mongoengine import Document, StringField, IntField, BooleanField
-
-class Pet(Document):
-    name = StringField(required=True)
-    species = StringField(required=True)
-    age = IntField(required=True)
-    owner = StringField(required=True)
-    vaccinated = BooleanField(default=False)
